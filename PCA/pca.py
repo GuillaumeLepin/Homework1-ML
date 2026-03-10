0bd313524b648770407fe8e806dcef3f75ecfa6a
nahuelArthur <160842802+nahuelArthur@users.noreply.github.com>
2026-03-10 14:02:52 +0100

    feat : start pca part

---
+++ PCA/pca.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+# Dataset
+B = np.array([
+    [2, 2, 4],
+    [2, 4, 6],
+    [4, 6, 10],
+    [4, 8, 12]
+])
+
+print("Dataset:")
+print(B)
+print("\n")
+
+
+# Step 1 : Standardize the data
+
+# Mean for each column
+mean = np.mean(B, axis=0)
+
+# DataSet centered 
+B_centered = B - mean
+
+print("Centered data:")
+print(B_centered)
+print("\n")
+
+
+
+
+# Step 2 : Covariance
+
+# Covariance data
+cov_matrix = np.cov(B_centered, rowvar=False)
+
+print("Covariance data:")
+print(cov_matrix)
+print("\n")
+
+
+# Step 3 : EigenVector 
+
+
+# Eigen decomposition
+eigenvalues, eigenvectors = np.linalg.eig(cov_matrix)
+
+print("Eigenvalues:")
+print(eigenvalues)
+print("\n")
